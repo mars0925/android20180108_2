e7f3285bbdb073ab1c28404fa7429192b89f76e9
MarsChang <mars09250148@yahoo.com.tw>
2018-01-08 11:44:55 +0800

    custom imageView in layout

--- app/src/main/java/com/mars/android20180108_2/MainActivity.java
+++ app/src/main/java/com/mars/android20180108_2/MainActivity.java
@@ -12,28 +12,34 @@ import java.util.Map;
 
 //listview 使用 Map來放資料
 public class MainActivity extends AppCompatActivity {
-    ArrayList<Map<String, String>> mylist = new ArrayList();
+    ArrayList<Map<String, Object>> mylist = new ArrayList();
     ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lv = (ListView) findViewById(R.id.listview);
-        HashMap<String, String> m1 = new HashMap<>();
+        //因為 HashMap要放字串以及整數,所以要改成object才能放
+        HashMap<String, Object> m1 = new HashMap<>();
         m1.put("city", "台北");
         m1.put("code", "02");
+        //R.drawable.tpi R 裡面的資源是用整數索引
+        m1.put("img",R.drawable.tpi);
         mylist.add(m1);
-        HashMap<String, String> m2 = new HashMap<>();
+        HashMap<String, Object> m2 = new HashMap<>();
         m2.put("city", "台中");
         m2.put("code", "04");
+        m2.put("img",R.drawable.tac);
         mylist.add(m2);
-        HashMap<String, String> m3 = new HashMap<>();
+        HashMap<String, Object> m3 = new HashMap<>();
         m3.put("city", "台南");
         m3.put("code", "06");
+        m3.put("img",R.drawable.tan);
         mylist.add(m3);
-        HashMap<String, String> m4 = new HashMap<>();
+        HashMap<String, Object> m4 = new HashMap<>();
         m4.put("city", "高雄");
         m4.put("code", "07");
+        m4.put("img",R.drawable.hk);
         mylist.add(m4);
 
         //android.R.layout.simple_list_item_2
@@ -46,9 +52,10 @@ public class MainActivity extends AppCompatActivity {
         */
 
         //自訂listview顯示格式
+        //在自訂的layout加入imageView可以顯示圖片
         SimpleAdapter adapter = new SimpleAdapter(MainActivity.this,
-                mylist, R.layout.mylistview, new String[] {"city", "code"},
-                new int[] {R.id.textView, R.id.textView2});
+                mylist, R.layout.mylistview, new String[] {"city", "code","img"},
+                new int[] {R.id.textView, R.id.textView2,R.id.imageView});
         lv.setAdapter(adapter);
 
 
